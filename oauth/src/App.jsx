cb6c87860ac13953961f86db09136ce335325923
cco <cco@lingqingmeng.com>
2024-01-19 17:08:57 -0500

    first com

---
+++ oauth/src/App.jsx
@@ -0,0 +1,31 @@
+
+
+import googleButton from './assets/google_signin_buttons/web/1x/btn_google_signin_dark_pressed_web.png'
+import './App.css'
+
+function navigate(url){
+  window.location.href = url;
+}
+
+async function auth(){
+  const response =await fetch('http://127.0.0.1:3000/request',{method:'post'});
+  const data = await response.json();
+  console.log(data);
+  navigate(data.url);
+}
+
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to LuciAttributor</h1>
+      <h3>Google OAuth!</h3>
+      <p>Visit <a href="https://twitter.com/luciaprotocol"><strong>@Luciaprotocol</strong></a> to see more great content!</p>
+      <button className="btn-auth"  type="button" onClick={()=> auth()}>
+        <img className="btn-auth-img" src={googleButton} alt='google sign in'/>
+      </button>
+    </>
+  )
+}
+
+export default App
